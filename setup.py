3943a9c5105d059e7d8724e628db1a204ea75428
mosiiisom <7407009+mosiiisom@users.noreply.github.com>
2025-08-26 13:49:10 +0000

    Bump version to 0.1.1 for PyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tpsms",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "requests>=2.28.0",  # For synchronous HTTP requests
